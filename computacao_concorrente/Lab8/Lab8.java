9c4e06cd470752dd54b3b0f4763352b3c7550878
VZuben <96436339+VZuben@users.noreply.github.com>
2022-06-23 20:39:44 -0300

    Update Lab8.java

--- computacao_concorrente/Lab8/Lab8.java
+++ computacao_concorrente/Lab8/Lab8.java
@@ -221,7 +221,7 @@ public class Lab8 {
 
       boolean corretudePy = false;                // Variável que controla se a impressão do log do programa vai ser .py ou não.
 
-      //se escrevermos "corretudePy" como argumento na hora de compilar, a impressão do log do programa vai ser .py 
+      //se escrevermos "corretudePy" como argumento na hora de executar, a impressão do log do programa vai ser .py 
       if(args.length > 0 && args[0].equals("corretudePy")) corretudePy = true;
       
       int i;
